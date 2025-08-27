7c99ca94196bbcc92dddd6ee492ad15109907711
abhimanoj <abhimanoj@gmail.com>
2025-08-27 17:36:08 +0530

    some

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import React, { useState } from 'react';
 import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
 import FAQPage from './pages/FAQPage';
-import CapabilitiesPage from './pages/CapabilitiesPage';
-import ArtworkGuidelinesPage from './pages/ArtworkGuidelinesPage';
+
 
 function App() {
   return (
@@ -16,12 +12,9 @@ function App() {
         <Header />
         <main>
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/faq" element={<FAQPage />} />
-            <Route path="/capabilities" element={<CapabilitiesPage />} />
-            <Route path="/artwork-guidelines" element={<ArtworkGuidelinesPage />} />
+
+            <Route path="/" element={<FAQPage />} />
+            
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
